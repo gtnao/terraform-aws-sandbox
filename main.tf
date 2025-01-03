c5d1d5aecff0510fd5cf3daeaf09fcfb7e2984b7
naotaka nakane <n.nakane0219@gmail.com>
2025-01-03 07:54:42 +0000

    Launch ec2.

--- main.tf
+++ main.tf
@@ -81,3 +81,45 @@ resource "aws_security_group" "ec2" {
     Name = "sandbox-ec2"
   }
 }
+
+// EC2
+resource "aws_instance" "dev1" {
+  ami                    = data.aws_ami.ubuntu_latest.id
+  instance_type          = "t3.medium"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+  key_name               = var.key_name
+
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "dev1"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
+}
+
+data "aws_ami" "ubuntu_latest" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "my_ips" {
   type = list(string)
 }
+
+variable "key_name" {
+  type = string
+}
