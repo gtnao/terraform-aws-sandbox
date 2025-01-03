56d3adc365ac6d57c9e22c40536d1c85d9b47a6d
naotaka nakane <n.nakane0219@gmail.com>
2025-01-03 06:44:00 +0000

    Add gha setting.

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = var.backend_bucket
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "backend_bucket" {
+  type = string
+}
+
 variable "my_ips" {
   type = list(string)
 }
